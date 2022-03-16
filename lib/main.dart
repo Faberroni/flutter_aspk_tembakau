3182d013e4e665c9c969dc8b08fd9f748d8c6d2d
Faberroni <raizal20ashari@apps.ipb.ac.id>
2022-03-17 04:21:01 +0700

    Refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'Page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         ).copyWith(
           secondary: Colors.green,
         ),
+        backgroundColor: Colors.lightBlue,
         textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.purple)),
         
       ),
@@ -29,65 +30,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _decrementCounter() {
-    setState(() => _counter--);
-  }
-  void _incrementCounter() {
-    setState(() => _counter++);
-  }
-  void _resetCounter() {
-    setState(() => _counter = 0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: const Icon(Icons.remove)),
-                ElevatedButton(
-                  onPressed: _incrementCounter, 
-                  child: const Icon(Icons.add))              
-              ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _resetCounter,
-        tooltip: 'Reset counter',
-        child: const Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}

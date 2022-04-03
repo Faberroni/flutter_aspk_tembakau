154a59b998e2a2e30b24a58e5346c001ef3e28e2
Gianluca <hanzwarraihan@apps.ipb.ac.id>
2022-04-03 14:08:04 +0700

    const verif

--- lib/constants/route.dart
+++ lib/constants/route.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_aspk_tembakau/Page/verify_email_page.dart';
+
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const inventoryRoute = '/inventory/';
+const VerifyEmailPage = '/verifyemail/';
+
